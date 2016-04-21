3e6578d2395f368b169e9b3ee4f0665e1a2bc742
Matt Hamel <matt@blispay.com>
2016-04-21 14:03:23 -0400

    bump

--- src/ios/Analytics-Bridging-Header.h
+++ src/ios/Analytics-Bridging-Header.h
@@ -1,2 +1,3 @@
 #import <Cordova/CDV.h>
 #import <Cordova/CDVPlugin.h>
+#import <Analytics/SEGAnalytics.h>
--- src/ios/AnalyticsPlugin.swift
+++ src/ios/AnalyticsPlugin.swift
@@ -1,5 +1,51 @@
+import Segment_GoogleAnalytics
+
 @objc(HWPAnalyticsPlugin) class AnalyticsPlugin: CDVPlugin {
-    func deviceReady(){
-        print("TEST FROM MH")
+    var analytics: SEGAnalytics!
+    let appVersion = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"] as! String
+
+    var userId: String!
+
+    func load(command: CDVInvokedUrlCommand){
+        let segConfig = SEGAnalyticsConfiguration(writeKey: command.arguments[0] as! String)
+        segConfig.flushAt = 1
+        SEGAnalytics.setupWithConfiguration(segConfig)
+    }
+    func page(command: CDVInvokedUrlCommand){
+        let name = command.argumentAtIndex(0, withDefault: nil)
+        if(name != nil && name as! String != ""){
+            SEGAnalytics.sharedAnalytics().screen(name as! String)
+        }
+    }
+    func track(command: CDVInvokedUrlCommand){
+        let eventName = command.arguments[0] as! String
+        let props = command.arguments[1] as? Dictionary<String, AnyObject>
+
+        if(props != nil){
+            SEGAnalytics.sharedAnalytics().track(eventName, properties: props!)
+        }else{
+            SEGAnalytics.sharedAnalytics().track(eventName)
+        }
+    }
+    func identify(command: CDVInvokedUrlCommand){
+        let userId = command.arguments[0] as? String
+        var traits = command.arguments[1] as? Dictionary<String, AnyObject> ?? [:]
+
+        traits["latest-ios-version"] = appVersion
+
+        if(userId != nil){
+            self.userId = userId
+        }
+
+        SEGAnalytics.sharedAnalytics().identify(self.getUserId(), traits: traits)
+    }
+
+    func getUserId() -> String{
+        if(self.userId != nil){
+            return self.userId
+        }
+        let uuid = NSUUID().UUIDString
+        self.userId = uuid
+        return self.userId
     }
 }
